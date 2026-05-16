9e56367b455075c24c97a424f4d52174a5350619
SakshyamKataria <sakshyamkataria@gmail.com>
2026-05-16 16:35:08 +0530

    Final fully automated deployment

--- src/portfolio.js
+++ src/portfolio.js
@@ -14,11 +14,10 @@ const greeting = {
   logo_name: "Sakshyam.p()",
   nickname: "Viper / picleric",
   full_name: "Sakshyam Kataria",
-  subTitle:
-    "Full Stack Developer, Machine Learning Enthusiast. Always learning.",
+  subTitle: "Full Stack Developer, DevOps Enthusiast. Always learning.",
   resumeLink:
     "https://acrobat.adobe.com/id/urn:aaid:sc:ap:0bb61bab-1db4-4907-a28f-34325cdcd7a0",
-  mail: "mailto:hrishipatel99@gmail.com",
+  mail: "mailto:sakshyamkataria@gmail.com",
 };
 
 const socialMediaLinks = {
